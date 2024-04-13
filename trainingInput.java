377d1528d5840bae84bf239b3cc854d231260a55
Evan12Scott <145315390+Evan12Scott@users.noreply.github.com>
2024-04-13 12:23:22 -0700

    Update trainingInput.java

--- trainingInput.java
+++ trainingInput.java
@@ -1,1 +1,42 @@
+/*
+Authors: Evan Scott, Kieran Kennedy, Sean Pala
+Last Date Modified: 4/10/24
+Description: trainingInput handles the retrieval of all necessary input from user that is required for training the hopfield net. Makes use of functions located in validateInput.java to ensure the validity of the input. 
+*/
+
+import java.util.Scanner;
+
+public class trainingInput {
+        validateInput userInput = new validateInput();
+	String readFile, weight, epoch, writeFile, learningRate, theta, threshold;
+	
+        /*
+	Description: prompts user for all necessary information to train the hopfield net
+	PARAMS: input: Scanner object
+	 	action: String convert to int
+	RETURN: None
+	*/
+	public void promptUser(Scanner input, String action) {
+		int typeAction = Integer.parseInt(action);
+
+		do {
+			System.out.println("Enter the training data file name and ensure it is located in the TRAININGSETS subdirectory:");
+                        readFile = input.nextLine();
+		} while(userInput.checkReadFile(readFile, typeAction));
+
+                do {
+                        System.out.println("Enter a file name to save the trained weight settings:");
+                        writeFile = input.nextLine();
+                        } while(userInput.validateWriteWeightsFile(writeFile));
+                
+                // add appropriate paths to the files provided
+                String readDataFile = "./trainingSets/" + readFile;
+                String writeWeightFile = "./trainedWeights/" + writeFile;
+
+                // all necessary inputs have been acquired so begin training of hopfield net
+		HopfieldTraining hopfieldTrain = new HopfieldTraining(readDataFile, writeWeightFile);
+		hopfieldTrain.Train();
+	}
+}
+
 
