11ae35aaf48db93ce6583eca1f925b0e7a0e41e3
Evan12Scott <145315390+Evan12Scott@users.noreply.github.com>
2024-04-13 12:22:58 -0700

    Update testingInput.java

--- testingInput.java
+++ testingInput.java
@@ -1,1 +1,48 @@
+/*
+Authors: Evan Scott, Kieran Kennedy, Sean Pala
+Last Date Modified: 3/7/24
+Description: Main method which abstracts the logic for the testing and training of perceptron net
+
+To run the program: 
+	javac validateInput.java testingInput.java trainingInput.java main.java PerceptronTesting.java  PerceptronTesting.java
+
+	java main
+
+*/
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String action, runAgain;
+
+		// execute until user specifies program termination
+		do {
+			validateInput userInput = new validateInput();
+			trainingInput train = new trainingInput();
+			testingInput test = new testingInput();
+
+			do {
+				System.out.println("Enter 1 to train using a training data file, enter 2 to use a trained weight settings data file:");
+				action = input.nextLine();
+			} while (userInput.checkAction(action));
+			
+			// train from data file then prompt subsequent testing
+			if(Integer.parseInt(action) == 1) {
+				train.promptUser(input, action);
+				test.promptUser(input, "2");
+			}
+
+			// test from data file with trained weights
+			else {
+				test.promptUser(input,action);
+			}
+
+			// program termination condition
+			System.out.println("Do you want to run the program again? (yes/no):");
+            runAgain = input.nextLine().toLowerCase();
+		} while(runAgain.equals("yes"));
+	}
+}
 
